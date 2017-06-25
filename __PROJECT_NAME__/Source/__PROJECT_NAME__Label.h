b43978e007c465abf58d8bcfc5eebbbe22312be8
Dmytro Zozulia <0xDmitry@gmail.com>
2017-06-25 05:35:52 +0200

    Added Objective-C Label (#37)

---
+++ __PROJECT_NAME__/Source/__PROJECT_NAME__Label.h
@@ -0,0 +1,13 @@
+//
+//  __PROJECT_NAME__Label.h
+//  __PROJECT_NAME__
+//
+//  Created by Dmytro Zozulia on 23.06.17.
+//  Copyright © 2017 __ORGANIZATION NAME__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface __PROJECT_NAME__Label : UILabel
+
+@end
