c9e11d1b95a042bab0d6bde7c0c85febabdf0154
William Entriken <github.com@phor.net>
2021-12-30 02:23:20 -0500

    update package syntax version

--- xxPROJECTxNAMExx/Package.swift
+++ xxPROJECTxNAMExx/Package.swift
@@ -1,28 +1,33 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "xxPROJECTxNAMExx",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "xxPROJECTxNAMExx",
-            targets: ["xxPROJECTxNAMExx"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["xxPROJECTxNAMExx"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "xxPROJECTxNAMExx",
-            dependencies: []),
+            path: "Sources"
+        ),
         .testTarget(
             name: "xxPROJECTxNAMExxTests",
-            dependencies: ["xxPROJECTxNAMExx"]),
-    ]
+            dependencies: ["xxPROJECTxNAMExx"],
+            path: "Tests",
+            exclude: ["CheckCocoaPodsQualityIndexes.rb"]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
