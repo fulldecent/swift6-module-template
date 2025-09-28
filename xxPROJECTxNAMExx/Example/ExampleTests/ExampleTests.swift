bfda7f0ede557ef453a4460e2d88b405de95123f
William Entriken <github.com@phor.net>
2025-09-28 14:15:33 -0400

    swift format

--- xxPROJECTxNAMExx/Example/ExampleTests/ExampleTests.swift
+++ xxPROJECTxNAMExx/Example/ExampleTests/ExampleTests.swift
@@ -6,12 +6,13 @@
 //
 
 import Testing
+
 @testable import Example
 
 struct ExampleTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
 
 }
